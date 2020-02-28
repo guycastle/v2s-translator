55da9c442984ddc157f3e666597a895c4fcf74bb
KieferB <kiefer.beernaert@outlook.com>
2020-02-28 12:18:53 +0100

    Initial commit with sample project to test if it runs on everyones computer

---
+++ android/app/src/main/kotlin/be/ehb/vts_trans/MainActivity.kt
@@ -0,0 +1,12 @@
+package be.ehb.vts_trans
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
